3fead82c2d592d73c3c4a6b28ac2576757cd0aaa
mohsen abasi <mohsentux@live.com>
2022-12-21 11:03:07 +0330

    some changes

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings:
         # Screen settings
         self.screen_width = 600
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.background_color = (230, 230, 230)
